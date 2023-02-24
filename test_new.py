7d31f1e970bd9e4b7acbbc18c556f90585c39e77
vmakhakhei <silenthealsqwe@gmail.com>
2023-02-24 22:01:53 +0100

    Apply autoformat

--- test_new.py
+++ test_new.py
@@ -9,11 +9,13 @@ def browser_size():
     browser.config.window_height = 1600
     browser.open('https://duckduckgo.com')
 
+
 def test_positive_search(browser_size):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[data-testid="result"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python - GitHub'))
+    browser.element('[data-testid="result"]').should(
+        have.text('yashaka/selene: User-oriented Web UI browser tests in Python - GitHub'))
+
 
 def test_negavite_search(browser_size):
     browser.element('[name="q"]').should(be.blank).type('olololkqwkew').press_enter()
     browser.element('[id="links_wrapper"]').should(have.text('По запросу olololkqwkew результаты не найдены.'))
-
